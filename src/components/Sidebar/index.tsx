362d7d50aa8a6569f6436626d02b7dc58e95e4bb
Tiago Cunha <tiago92cunha@gmail.com>
2022-10-09 12:04:37 +0100

    Add sidebar sections

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,28 +1,31 @@
 import React, { useState, PropsWithChildren } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { faHouse, faAnglesLeft } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faAnglesLeft, faPlus, faSearch, faPencil } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
 
 export const Sidebar: React.FC = () => {
   const [open, setOpen] = useState<boolean>(false);
   return (
-    <div className={`h-full bg-white rounded-xl ${open ? 'w-60' : 'w-18'} flex flex-col gap-2`}>
-      <SidebarSection>
+    <div className={`h-full bg-white rounded-xl ${open ? 'w-60' : 'w-18'} flex flex-col gap-2 py-2`}>
+      <div className=' flex justify-center'>
         <FontAwesomeIcon
           icon={faAnglesLeft}
           className={`bg-neutral-100 text-neutral-500 h-3 w-3 rounded-full p-3 flex ${
-            open ? 'ml-auto' : ''
+            open ? 'ml-auto mr-3' : ''
           } hover:cursor-pointer hover:text-[#613aeb] hover:bg-[#efebfe]`}
           onClick={() => setOpen(!open)}
         ></FontAwesomeIcon>
-      </SidebarSection>
-      <SidebarSection>
+      </div>
+
+      <SidebarSection open={open}>
         <SidebarItem text='Dashboard' icon={faHouse} open={open} />
       </SidebarSection>
       <SidebarSeparator />
-      <SidebarSection title='MANAGEMENT'>
-        <SidebarItem text='Create New Catalog' icon={faHouse} open={open} />
+      <SidebarSection title='MANAGEMENT' open={open}>
+        <SidebarItem text='Create New Catalog' icon={faPlus} open={open} />
+        <SidebarItem text='List Categories' icon={faSearch} open={open} />
+        <SidebarItem text='Edit Existing Catalog' icon={faPencil} open={open} />
       </SidebarSection>
     </div>
   );
@@ -32,10 +35,12 @@ const SidebarItem: React.FC<{ icon: IconProp; text: string; open: boolean }> = (
   return (
     <Link href={'/'}>
       <div
-        className={`flex h-10 items-center gap-3 p-2.5 text-neutral-500 hover:text-[#613aeb] hover:bg-[#efebfe] hover:cursor-pointer rounded-md`}
+        className={`flex h-10  items-center gap-3 p-2.5 text-neutral-500 hover:text-[#613aeb] hover:bg-[#efebfe] hover:cursor-pointer rounded-md ${
+          open ? '' : 'justify-center w-10'
+        }`}
       >
-        <FontAwesomeIcon icon={icon} className='text-base ' />
-        <span className={`${open ? 'block' : 'hidden'} text-base`}>{text}</span>
+        <FontAwesomeIcon icon={icon} className='text-base' />
+        <span className={`${open ? 'block' : 'hidden'} text-sm`}>{text}</span>
       </div>
     </Link>
   );
@@ -47,12 +52,13 @@ const SidebarSeparator: React.FC = () => {
 
 type SidebarSectionProps = {
   title?: string;
+  open: boolean;
 };
 
-const SidebarSection: React.FC<PropsWithChildren<SidebarSectionProps>> = ({ title, children }) => {
+const SidebarSection: React.FC<PropsWithChildren<SidebarSectionProps>> = ({ title, open, children }) => {
   return (
-    <div className='p-3'>
-      {/* {title && <b className={`p-2.5 text-neutral-500 text-[8px]`}>{title}</b>} */}
+    <div className={`p-3 flex flex-col ${open ? '': 'items-center'}`}>
+      {title && <b className={`p-2.5 text-[#613aeb] text-[8px]`}>{title}</b>}
       {children}
     </div>
   );
