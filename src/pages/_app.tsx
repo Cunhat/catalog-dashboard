f63d258412c03a2be21a95bcd3b7f8a6feea06d0
Tiago Cunha <tiago92cunha@gmail.com>
2022-10-16 16:14:22 +0100

    Fix flash fontawsome icons on loading page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/globals.css';
 import { AppPropsWithLayout } from '@/types';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page);
