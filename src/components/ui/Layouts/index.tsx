be2b879d846270dac462545e752971e1644f241c
Tiago Miguel Cunha <tiago92cunha@gmail.com>
2022-11-24 22:40:18 +0000

    Add Menu component and the possibility to logout

--- src/components/ui/Layouts/index.tsx
+++ src/components/ui/Layouts/index.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Sidebar } from '@ui/Sidebar';
 import { ActionBar } from '@ui/ActionBar';
 import { Text } from '@ui/Typography/Text';
-import { faBell, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faChevronDown, faGear, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { PopoverMenu } from '@ui/PopoverMenu';
 import { Toggle } from '@ui/Toggle';
 import { useAtom, atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
+import { Menu, type MenuElement } from '@ui/Menu';
+import { signOut } from 'next-auth/react';
 
 export const darkModeAtom = atom(false);
 
@@ -26,12 +28,29 @@ export const DashboardLayout: FC<PropsWithChildren> = ({ children }) => {
     // console.log(darkMode);
   }, [darkMode]);
 
+  const data: Array<MenuElement> = [
+    {
+      label: 'Log out',
+      href: '/logout',
+      type: 'button',
+      onClick: () => signOut(),
+      icon: faRightFromBracket,
+    },
+    {
+      label: 'User settings',
+      href: '/settings',
+      type: 'link',
+      icon: faGear,
+    },
+  ];
+
   return (
     <div className='box-border flex h-screen flex-col bg-slate-100 dark:bg-dark-background'>
       <nav className='flex h-[50px] w-full bg-white shadow-lg items-center justify-end pr-5 gap-3 dark:bg-dark-widget'>
         <DarkThemeSwitcher />
         <FontAwesomeIcon icon={faBell} className='text-lg text-neutral-500 hover:cursor-pointer hover:text-primary' />
-        <UserCard></UserCard>
+        {/* <UserCard></UserCard> */}
+        <Menu data={data}></Menu>
       </nav>
       <div className='flex flex-1 gap-3 overflow-auto p-3'>
         <Sidebar />
@@ -46,18 +65,21 @@ export const DashboardLayout: FC<PropsWithChildren> = ({ children }) => {
 
 const UserCard = () => {
   const [open, setOpen] = React.useState<boolean>(false);
+
   return (
-    <div className='flex gap-3 hover:cursor-pointer hover:bg-hover py-1 px-3 rounded-md' onClick={() => setOpen(!open)}>
-      <div className='rounded-full bg-hover p-[6px] flex justify-center items-center border border-primary'>
-        <span className='text-sm text-primary font-bold m-0 p-0'>US</span>
-      </div>
-      <div className='flex flex-col items-center justify-center'>
-        <p className='text-xs text-neutral-500 dark:text-white'>User Name</p>
-      </div>
-      {/* <div className='flex justify-center items-center'>
+    <>
+      <div className='flex gap-3 hover:cursor-pointer hover:bg-hover py-1 px-3 rounded-md' onClick={() => setOpen(!open)}>
+        <div className='rounded-full bg-hover p-[6px] flex justify-center items-center border border-primary'>
+          <span className='text-sm text-primary font-bold m-0 p-0'>US</span>
+        </div>
+        <div className='flex flex-col items-center justify-center'>
+          <p className='text-xs text-neutral-500 dark:text-white'>User Name</p>
+        </div>
+        {/* <div className='flex justify-center items-center'>
         <FontAwesomeIcon icon={faChevronDown} className='text-sm text-neutral-500 hover:cursor-pointer hover:text-primary' />
       </div> */}
-    </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/ui/Menu/index.tsx
@@ -0,0 +1,70 @@
+import { Fragment } from 'react';
+import { Menu as MenuComponent, Transition } from '@headlessui/react';
+import Link from 'next/link';
+import { cva } from 'class-variance-authority';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const MenuStyles = cva(
+  'hover:bg-primary flex gap-3 items-center text-left hover:text-white text-neutral-500 dark:text-white p-2 rounded-md hover:cursor-pointer',
+);
+
+export type MenuElement = {
+  label: string;
+  href: string;
+  type: 'link' | 'button';
+  onClick?: () => void | Promise<undefined>;
+  icon?: IconProp;
+};
+
+type MenuProps = {
+  data: Array<MenuElement>;
+};
+
+export const Menu: React.FC<MenuProps> = ({ data }) => {
+  return (
+    <MenuComponent as='div' className='relative inline-block text-left'>
+      <MenuComponent.Button as={UserMenu} />
+      <Transition
+        as={Fragment}
+        enter='transition ease-out duration-100'
+        enterFrom='transform opacity-0 scale-95'
+        enterTo='transform opacity-100 scale-100'
+        leave='transition ease-in duration-75'
+        leaveFrom='transform opacity-100 scale-100'
+        leaveTo='transform opacity-0 scale-95'
+      >
+        <MenuComponent.Items className='absolute p-2 right-0 mt-2 flex flex-col min-w-[200px] w-auto origin-top-right rounded-md bg-white dark:bg-dark-background shadow-xl  focus:outline-none'>
+          {data.map((elem) => {
+            return elem.type === 'link' ? (
+              <MenuComponent.Item as={Link} href={elem.href} key={'href'}>
+                <div className={MenuStyles()}>
+                  {elem.icon && <FontAwesomeIcon icon={elem.icon} />}
+                  <span>{elem.label}</span>
+                </div>
+              </MenuComponent.Item>
+            ) : (
+              <MenuComponent.Item as='button' className={MenuStyles()} onClick={elem.onClick}>
+                {elem.icon && <FontAwesomeIcon icon={elem.icon} />}
+                <span>{elem.label}</span>
+              </MenuComponent.Item>
+            );
+          })}
+        </MenuComponent.Items>
+      </Transition>
+    </MenuComponent>
+  );
+};
+
+const UserMenu = () => {
+  return (
+    <MenuComponent.Button className='flex gap-3 hover:cursor-pointer hover:bg-hover py-1 px-3 rounded-md items-center justify-center'>
+      <div className='rounded-full bg-hover p-[6px] flex justify-center items-center border border-primary'>
+        <span className='text-sm text-primary font-bold m-0 p-0'>US</span>
+      </div>
+      <div className='flex flex-col '>
+        <p className='text-xs text-neutral-500 dark:text-primary'>User Name</p>
+      </div>
+    </MenuComponent.Button>
+  );
+};
