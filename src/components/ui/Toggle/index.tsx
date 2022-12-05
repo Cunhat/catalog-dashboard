c9d40e3aa41508d3a2d517233fb0797996f71941
Tiago Miguel Cunha <tiago92cunha@gmail.com>
2022-12-05 18:57:20 +0000

    Refactor dark theme toggle

--- src/components/ui/Toggle/index.tsx
+++ src/components/ui/Toggle/index.tsx
@@ -1,6 +1,6 @@
 import { Switch } from '@headlessui/react';
 import { Fragment } from 'react';
-import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 
@@ -12,12 +12,7 @@ export const Toggle: React.FC<{ enabled: boolean; setEnabled: () => void }> = ({
         <button className={`${checked ? 'bg-dark-background' : 'bg-gray-200'} relative inline-flex h-6 w-11 items-center rounded-full`}>
           <span className='sr-only'>Enable notifications</span>
           {checked ? (
-            <FontAwesomeIcon
-              icon={checked ? faMoon : faSun}
-              className={`${
-                checked ? 'translate-x-6' : 'translate-x-1'
-              } inline-block h-4 w-4 transform rounded-full text-primary  transition`}
-            />
+            <FontAwesomeIcon icon={faMoon} className='translate-x-6 inline-block h-4 w-4 transform rounded-full text-primary  transition' />
           ) : (
             <Image src={'/img/sun.svg'} alt='sun' width={20} height={20} className={`translate-x-0 transform   transition`} />
           )}
